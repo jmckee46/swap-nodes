d8673ae3a3dd7ef6e29dc38b6c5fb2901ef52b18
John McKee <jmckee3@mac.com>
2020-04-25 16:14:39 -0700

    initial commit

---
+++ spec-swap-nodes_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSwapNodes1(t *testing.T) {
+
+	fmt.Println("Answer should be: 1 4")
+}
---
+++ swap-nodes.go
@@ -0,0 +1,10 @@
+package main
+
+func swapNodes(indexes [][]int32, queries []int32) [][]int32 {
+	/*
+	 * Write your code here.
+	 */
+	return nil
+}
+
+func main() {}
